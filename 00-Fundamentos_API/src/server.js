30c9f6a918bf16b22398b28c0ae9f5f290839a94
Natan Almeida <natanalmeida158@hotmail.com>
2025-06-18 11:18:10 -0300

    create API

---
+++ 00-Fundamentos_API/src/server.js
@@ -0,0 +1,17 @@
+import http from "node:http"
+
+const server = http.createServer((request, response) => {
+    const { method, url } = request
+
+    if (method === "GET" && url === "/products") {
+        return response.end("Lista de produtos!")
+    }
+
+    if (method === "POST" && url === "/products") {
+        return response.writeHead(201).end("Produto cadastrado!")
+    }
+    
+    return response.writeHead(404).end("Rota não encontada!")
+})
+
+server.listen(3333)
