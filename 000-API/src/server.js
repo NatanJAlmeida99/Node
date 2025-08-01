7e35eb10747b8880b4eac05bc61ca64e4f077369
Natan Almeida <natanalmeida158@hotmail.com>
2025-08-01 11:56:25 -0300

    recuperando dados

---
+++ 000-API/src/server.js
@@ -0,0 +1,25 @@
+import http from "node:http"
+
+const server = http.createServer(async (request, response) => {
+    const { method, url } = request
+
+    if (method === "GET" && url === "/products") {
+        return response.end("Lista de produtos")
+    }
+
+    if (method === "POST" && url == "/products") {
+        const buffers = []
+
+        for await (const chunk of request) {
+            buffers.push(chunk)
+        }
+
+        console.log(Buffer.concat(buffers).toString())
+
+        return response.writeHead(201).end("Produto cadastrado!")
+    }
+
+    return response.writeHead(404).end("Rota não encontrada!")
+})
+
+server.listen(3333)
